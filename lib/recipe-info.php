98f0bf3ed71de066ae7281d211dcbd89f0c93065
KimLamers <kiml_1194@hotmail.com>
2023-08-02 12:01:30 +0200

    Vervaardig recipe info class DRAFT (#8)

--- lib/recipe-info.php
+++ lib/recipe-info.php
@@ -27,22 +27,57 @@ class recipe_info {
 
         $recipeInfoArray = [];
 
-        while($recipe_info = mysqli_fetch_array($result)){
+        while($recipe_info = mysqli_fetch_array($result)) {
             
             // if record type is O
+            if($recipe_info['record_type'] === 'O') { 
 
                 // get user
+                $user = $this->selectUser($recipe_info['user_id'], MYSQLI_ASSOC);
+
+                $recipeInfoArray[] = [
+                    "id" => $recipe_info['id'],
+                    "record_type" => $recipe_info['record_type'],
+                    "recipe_id" => $recipe_info['recipe_id'],
+                    "user_id" => $recipe_info['user_id'],
+                    "date" => $recipe_info['date'],
+                    "number_field" => $recipe_info['number_field'],
+                    "text_field" => $recipe_info['text_field'],
+                    "username" => $user['username'],
+                    "password" => $user['password'],
+                    "email" => $user['email'],
+                    "user_image"=> $user['user_image'],
+                ];
 
             // elseif record type is F
+            } elseif($recipe_info['record_type'] === 'F') {
 
                 // get user
+                $user = $this->selectUser($recipe_info['user_id'], MYSQLI_ASSOC);
 
-            // else
-            
-                // no need for user
+                $recipeInfoArray[] = [
+                    "id" => $recipe_info['id'],
+                    "record_type" => $recipe_info['record_type'],
+                    "recipe_id" => $recipe_info['recipe-id'],
+                    "user_id" => $recipe_info['user_id'],
+                    "date" => $recipe_info['date'],
+                    "number_field" => $recipe_info['number_field'],
+                    "text_field" => $recipe_info['text_field'],
+                    "username" => $user['username'],
+                    "password" => $user['password'],
+                    "email" => $user['email'],
+                    "user_image" => $user['user_image'],
+                ];
 
+            // else record type is not O or F
+            } else {
 
+                // no need to retrieve user
+                echo "test";
+            }
         }
+
+        return($recipeInfoArray);
 
     }
     
