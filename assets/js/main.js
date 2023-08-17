6ceca3115483344aedbe072203866b314e759a73
KimLamers <kiml_1194@hotmail.com>
2023-08-17 08:24:39 +0200

    Frontend DRAFT v3 (#13)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,16 @@
+let isOpen = false;
+let isAnimating = false;
+
+// $('.header__menu-trigger-holder').click(() => {
+//     console.log('click');
+//     console.log($('.header__menu-holder'));
+//     $('.header__menu-holder').css({
+//         transform: isOpen ? 'translateY(-100%)' : 'translateY(0)'
+//     }, 1000);
+
+// });
+
 $('.header__menu-trigger-holder').click(() => {
-    console.log('click');
-    console.log($('.header__menu-holder'));
-    $('.header__menu-holder').css({
-        transform: 'translateY(0)'
-    }, 1000);
+    $('.header__menu-holder').toggleClass('header__menu-holder--is-open');
 
 });
