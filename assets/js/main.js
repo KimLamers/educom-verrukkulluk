ec918fa58205ac3d8daa956099356036c2052f2f
KimLamers <kiml_1194@hotmail.com>
2023-08-24 08:55:00 +0200

    Frontend DRAFT v10 (#13)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,31 @@
+/* NAVIGATION */
 let isOpen = false;
 let isAnimating = false;
-
-// $('.header__menu-trigger-holder').click(() => {
-//     console.log('click');
-//     console.log($('.header__menu-holder'));
-//     $('.header__menu-holder').css({
-//         transform: isOpen ? 'translateY(-100%)' : 'translateY(0)'
-//     }, 1000);
-
-// });
 
 $('.header__menu-trigger-holder').click(() => {
     $('.header__menu-holder').toggleClass('header__menu-holder--is-open');
 
 });
+
+/* RECIPE DETAIL TABLE */
+let previousTab = document.querySelector('[data-tab]');
+let previousButton = document.querySelector('.tablinks');
+
+function openTab(event, tabName) {
+    const tabContent = document.querySelector(`[data-tab="${tabName}"]`);
+
+    if (tabContent.classList.contains('active')) {
+        return;
+    }
+
+    tabContent.classList.add('active');
+    event.currentTarget.classList.add('active');
+
+    previousTab.classList.remove('active');
+    previousTab = tabContent;
+    
+    previousButton.classList.remove('active');
+    previousButton = event.currentTarget;
+
+}
+
