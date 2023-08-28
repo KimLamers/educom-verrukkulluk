70835e588cd16d1793eee65f2b25fe463dcaf431
KimLamers <kiml_1194@hotmail.com>
2023-08-28 16:59:42 +0200

    Frontend DRAFT v13 (#13)

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 //// Allereerst zorgen dat de "Autoloader" uit vendor opgenomen wordt:
-require_once("E:/Tijdelijke downloads/Programming/XAMPP/XAMPP/htdocs/vendor/autoload.php");
+require_once("/Users/Kimmie/Projecten/Software/vendor/autoload.php");
 
 /// Twig koppelen:
-$loader = new \Twig\Loader\FilesystemLoader("E:/Tijdelijke downloads/Programming/XAMPP/XAMPP/htdocs/educom-verrukkulluk/templates");
+$loader = new \Twig\Loader\FilesystemLoader("/Users/Kimmie/Projecten/educom-verrukkulluk/templates");
 /// VOOR PRODUCTIE:
 /// $twig = new \Twig\Environment($loader), ["cache" => "./cache/cc"]);
 
@@ -16,9 +16,12 @@ $twig->addExtension(new \Twig\Extension\DebugExtension());
 /// Next step, iets met je data doen. Ophalen of zo
 require_once("lib/database.php");
 require_once("lib/recipe.php");
+require_once("lib/ingredient.php");
 $db = new database();
 $recipe = new recipe($db->getConnection());
-$data = $recipe->selectRecipeById(1);
+$ingredient = new ingredient($db->getConnection());
+$data = $recipe->selectRecipeById();
+$ingredient_data = $ingredient->selectIngredient(1);
 
 
 /*
@@ -26,24 +29,14 @@ URL:
 http://localhost/index.php?recipe_id=4&action=detail
 */
 
-$recipe_id = isset($_GET["id"]) ? $_GET["id"] : "";
-$kitchen_id = isset($_GET["kitchen_id"]) ? $_GET["kitchen_id"] : "";
-$type_id = isset($_GET["type_id"]) ? $_GET["type_id"] : "";
-$user_id = isset($_GET["user_id"]) ? $_GET["user_id"] : "";
-$date_added = isset($_GET["date_added"]) ? $_GET["date_added"] : "";
-$recipe_title = isset($_GET["recipe_title"]) ? $_GET["recipe_title"] : "";
-$recipe_description_short = isset($_GET["recipe_description_short"]) ? $_GET["recipe_description_short"] : "";
-$recipe_description_long = isset($_GET["recipe_description_long"]) ? $_GET["recipe_description_long"] : "";
-$recipe_image = isset($_GET["recipe_image"]) ? $_GET["recipe_image"] : "";
-
-
+$recipe_id = isset($_GET["id"]) ? $_GET["id"] : null;
 $action = isset($_GET["action"]) ? $_GET["action"] : "homepage";
 
 
 switch($action) {
 
         case "homepage": {
-            $data = $recipe->selectRecipeById(1); // recipe_id
+            $data = $recipe->selectRecipeById(); // recipe_id
             $template = 'homepage.html.twig';
             $title = "homepage";
             break;
@@ -51,17 +44,18 @@ switch($action) {
 
         case "detail": {
             $data = $recipe->selectRecipeById(1); // recipe_id
+            $ingredient_data = $ingredient->selectIngredient(1); // recipe_id
             $template = 'detail.html.twig';
             $title = "detail pagina";
             break;
         }
 
-        case "shoppinglist": {
-            $data = $recipe->selectRecipeById(1); // recipe_id
-            $template = 'shoppinglist.html.twig';
-            $title = "shopping list";
-            break;
-        }
+        // case "shoppinglist": {
+        //     $data = $recipe->selectRecipeById(1); // recipe_id
+        //     $template = 'shoppinglist.html.twig';
+        //     $title = "shopping list";
+        //     break;
+        // }
 
         // case "add_rating": {
         //     $data = $rating->selectRating(1); // recipe_id
@@ -102,4 +96,4 @@ $template = $twig->load($template);
 
 
 /// En tonen die handel!
-echo $template->render(["title" => $title, "data" => $data]);
+echo $template->render(["title" => $title, "data" => $data, "ingredient_data" => $ingredient_data]);
--- lib/recipe.php
+++ lib/recipe.php
@@ -52,56 +52,36 @@ class recipe
     }
 
 
-    public function selectRecipeById($recipe_id) {
+    public function selectRecipeById($recipe_id = NULL) {
 
-        // get list of function arguments from user and put into array
-        $argList = func_get_args();
-        
-
-        // link argList elements to $recipe_id
-        foreach($argList as $key => $value) {
-            $recipe_id = ($argList);
+        $sql = "SELECT * FROM recipe";
+        if(!is_null($recipe_id)) {
+            $sql .= " WHERE id  = $recipe_id";
         }
 
-        // clean data
-        // $recipe_id = mysqli_real_escape_string($this->connection, $recipe_id);
-        
-
-        // sql query, loop over results
-        foreach ($recipe_id as $key => $value) {
-        $sql = "SELECT * FROM recipe WHERE id = $value";
         $result = mysqli_query($this->connection, $sql);
-        $resultArray[] = $result;
         
-
-        // sql query ingredients
-        $sql_ingredients = "SELECT * FROM ingredients WHERE recipe_id = $value";
-        $resuls_ingredients = mysqli_query($this->connection, $sql_ingredients);
-
-        
-        $recipe = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        if (!$result) {
-            return NULL;
-            } 
-
-
-        foreach ($recipe as $key => $value) {
+        while($recipe = mysqli_fetch_array($result)) { 
             // get user
-            $user = $this->selectUser($value['user_id'], MYSQLI_ASSOC);
+            $user = $this->selectUser($recipe['user_id']);
             // get kitchen
-            $kitchen = $this->selectKitchenType($value['kitchen_id'], MYSQLI_ASSOC);
+            $kitchen = $this->selectKitchenType($recipe['kitchen_id']);
             // get type
-            $type = $this->selectKitchenType($value['type_id'], MYSQLI_ASSOC);
+            $type = $this->selectKitchenType($recipe['type_id']);
+            // get ingredients
+            $ingredients = $this->selectIngredient($recipe['id']);
+            
             $recipeArray[] = [
-                "recipe_id" => $value['id'],
-                "kitchen_id" => $value['kitchen_id'],
-                "type_id" => $value['type_id'],
-                "user_id" => $value['user_id'],
-                "date_added" => $value['date_added'],
-                "recipe_title" => $value['recipe_title'],
-                "recipe_description_short" => $value['recipe_description_short'],
-                "recipe_description_long" =>  $value['recipe_description_long'],
-                "recipe_image" =>  $value['recipe_image'],
+                "recipe_id" => $recipe['id'],
+                "kitchen_id" => $recipe['kitchen_id'],
+                "type_id" => $recipe['type_id'],
+                "user_id" => $recipe['user_id'],
+                "date_added" => $recipe['date_added'],
+                "recipe_title" => $recipe['recipe_title'],
+                "recipe_description_short" => $recipe['recipe_description_short'],
+                "recipe_description_long" =>  $recipe['recipe_description_long'],
+                "recipe_image" =>  $recipe['recipe_image'],
+                "number_of_people" => $recipe['number_of_people'],
                 "username" => $user['username'],
                 "password" => $user['password'],
                 "email" => $user['email'],
@@ -110,33 +90,12 @@ class recipe
                 "description_kitchen" => $kitchen['description'],
                 "record_type_type" => $type['record_type'],
                 "description_type" => $type['description'],
+                "ingredients" => $ingredients,
             ];
-            // get ingredient and articles
-            $ingredient = $this->selectIngredient($value['id'], MYSQLI_ASSOC);
-            while ($ingredient = mysqli_fetch_array($resuls_ingredients)) {
-                $recipeArray[] = [
-                    "ingredient_id" => $ingredient['id'],
-                    "article_id" => $ingredient['article_id'],
-                    "number" => $ingredient['number'],
-                ];
-                // get article
-                $article = $this->selectArticle($ingredient['article_id'], MYSQLI_ASSOC);
-                $recipeArray[] = [
-                    "article_id" => $article['id'],
-                    "article_name" => $article['article_name'],
-                    "article_description" => $article['article_description'],
-                    "article_price" => $article['article_price'],
-                    "article_unit" => $article['article_unit'],
-                    "article_packaging" => $article['article_packaging'],
-                    "article_calories" => $article['article_calories'],
-                ];
-            
-            }
 
         }
-    }
-    
-    
+        
+        //echo '<pre>'; print_r($recipeArray); echo '</pre>';
         return ($recipeArray);
     }
 
